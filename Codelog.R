7e75253c0e7a3ef902834e5766b25bf58ff2ffa3
Calvin <87352301+Calvin-LLC@users.noreply.github.com>
2025-11-13 12:11:57 -0600

    more r code

--- Codelog.R
+++ Codelog.R
@@ -4,3 +4,16 @@ data <- na.omit(data)
 
 # Filter to include only approved (A) gage height readings
 data <- subset(data, gage_height_quality == "A")
+
+# Remove anomalies (negative or zero values that shouldn't exist in streamflow)
+data <- subset(data, discharge > 0 & gage_height > 0)
+
+# Outlier detection using z-scores (flag values beyond 3 standard deviations)
+data$discharge_z <- scale(data$discharge)
+data$gage_height_z <- scale(data$gage_height)
+
+# Remove outliers
+data <- subset(data, abs(discharge_z) <= 3 & abs(gage_height_z) <= 3)
+
+# Drop z-score columns and unnecessary columns
+data <- subset(data, select = -c(discharge_z, gage_height_z, agency, timezone))
